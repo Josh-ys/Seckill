ccf391b3bb4016d9cbeac97955ea33cd335a8e99
Reasonless <ysh.c@foxmail.com>
2018-05-27 21:28:12 +0800

    start

---
+++ src/main/java/com/seckill/ysh/seckill/Application.java
@@ -0,0 +1,12 @@
+package com.seckill.ysh.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
