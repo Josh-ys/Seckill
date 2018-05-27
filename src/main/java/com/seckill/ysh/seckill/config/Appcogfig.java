61d03aa7a0eb52e6819f5356144f895c59bfc166
Reasonless <ysh.c@foxmail.com>
2018-05-27 22:47:26 +0800

    配置读写的添加

---
+++ src/main/java/com/seckill/ysh/seckill/config/Appcogfig.java
@@ -0,0 +1,28 @@
+package com.seckill.ysh.seckill.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author joey
+ * @date 2018/05/27 21:30
+ */
+@Getter
+@Configuration
+@PropertySource(value = "classpath:application.properties")
+public class Appcogfig {
+
+    @Autowired
+    private Environment environment;
+
+    public String getValue(String key) {
+        return environment.getProperty(key);
+    }
+
+    public <T> T getValue(String key, Class<T> clazz) {
+        return environment.getProperty(key, clazz);
+    }
+}
